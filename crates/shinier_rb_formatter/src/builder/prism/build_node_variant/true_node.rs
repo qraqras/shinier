331b1536f05699ba8bb96f3d3287f57bec3543c8
qraqras <q100rrraaa@gmail.com>
2025-11-26 12:12:45 +0000

    fix: break_node

--- crates/shinier_rb_formatter/src/builder/prism/build_node_variant/true_node.rs
+++ crates/shinier_rb_formatter/src/builder/prism/build_node_variant/true_node.rs
@@ -1,12 +1,8 @@
-// filepath: /workspaces/shinier/crates/shinier_rb_formatter/src/builder/prism/new_build_node_variant/true_node.rs
-
 use crate::Document;
-use crate::builder::builder::*;
 use crate::builder::prism::BuildContext;
-use crate::builder::prism::build_node::build_node;
-use crate::keyword::*;
-use ruby_prism::*;
+use crate::builder::prism::build_location::build_node_as_location;
+use ruby_prism::TrueNode;
 
 pub fn build_true_node(node: &TrueNode<'_>, ctx: &mut BuildContext) -> Option<Document> {
-    None
+    build_node_as_location(&node.as_node(), ctx)
 }
--- crates/shinier_rb_formatter/tests/ruby/node_variants/call_node.rb
+++
@@ -1,8 +0,0 @@
-foo(a, b, c)
-foo a, b, c
-
-foo aaaaaaaaaaaa, bbbbbbbbbbb, ccccccccccc, ddddddddddddddddddddddddd, eeeeeeeeeeeeeeeeeeeeeeeeee
-
-foo()
-
-1 + 2 + 3 + 4
