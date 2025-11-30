00b000c3957bf948da19fc87cc79a9002599d084
qraqras <q100rrraaa@gmail.com>
2025-11-30 07:14:26 +0000

    fix: class_variable_or_write_node

--- crates/shinier_rb_formatter/src/builder/prism/build_node_variant/class_variable_operator_write_node.rs
+++ crates/shinier_rb_formatter/src/builder/prism/build_node_variant/class_variable_operator_write_node.rs
@@ -1,15 +1,23 @@
-// filepath: /workspaces/shinier/crates/shinier_rb_formatter/src/builder/prism/new_build_node_variant/class_variable_operator_write_node.rs
-
 use crate::Document;
 use crate::builder::BuildContext;
 use crate::builder::builder::*;
+use crate::builder::prism::build_location::build_location;
 use crate::builder::prism::build_node::build_node;
-use crate::keyword::*;
-use ruby_prism::*;
+use ruby_prism::ClassVariableOperatorWriteNode;
 
+/// Builds ClassVariableOperatorWriteNode.
 pub fn build_class_variable_operator_write_node(
     node: &ClassVariableOperatorWriteNode<'_>,
     ctx: &mut BuildContext,
 ) -> Option<Document> {
-    None
+    let name_loc = node.name_loc();
+    let binary_operator_loc = node.binary_operator_loc();
+    let value = node.value();
+
+    group(array(&[
+        build_location(name_loc, ctx),
+        space(),
+        build_location(binary_operator_loc, ctx),
+        indent(array(&[line(), build_node(value, ctx)])),
+    ]))
 }
---
+++ crates/shinier_rb_formatter/tests/ruby/node_variants/class_variable_or_write_node.rb
@@ -0,0 +1,34 @@
+@@target ||= value
+
+# unnecessary line breaks
+@@target ||=
+  value
+
+# long expressions
+@@target ||= very_long_expression_that_exceeds_line_length_very_long_expression_that_exceeds_line_length
+
+@@target ||= # comment
+  value
+
+@@target ||=
+  # comment
+  value
+
+@@target ||=
+  value # comment
+
+@@target ||= # comment
+  # comment
+  value
+
+@@target ||= # comment
+  value # comment
+
+@@target ||=
+  # comment
+  value # comment
+
+@@target ||= # comment
+  # comment
+  value # comment
+  # comment
